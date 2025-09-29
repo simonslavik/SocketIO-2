c4961ba72cc7c28d1ca3aa466e4c2a7aa6dfccf1
simonslavik <simonslavik007@gmail.com>
2025-09-29 14:15:04 +0200

    commited

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {cors: {origin: "*"}});
+
+
+app.set("view engine", "ejs");
+
+app.get("/home", (req, res) => {
+    res.render("home");
+})
+
+server.listen(3001, () => {
+    console.log("Server is running on port 3001");
+})
+
+io.on("connection", (socket) => {
+
+    console.log("User connected:", socket.id);
+
+    socket.on("message", (data) => {
+        socket.broadcast.emit("message", data);
+    })
+
+
+})
